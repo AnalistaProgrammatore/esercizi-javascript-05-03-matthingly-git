17e1871dc696a78dd0259e2c4a6050937d25cb3c
matthingly-git <matthew.cesar96@gmail.com>
2020-03-14 22:38:15 +0100

    Ok

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,30 @@
 
+function student () {
+  this.grades = []
+  this.addGrade = addGrade
+  this.average = average
+}
+
+function addGrade (grade) {
+  this.grades.push(grade)
+}
+
+function average () {
+  var total = 0
+  for (var i = 0; i < this.grades.length; i++) {
+    total += this.grades[i]
+  }
+  return total / this.grades.length
+}
+
+var Matthew = new student()
+
+Matthew.addGrade(98)
+Matthew.addGrade(78)
+Matthew.addGrade(68)
+Matthew.addGrade(52)
+Matthew.addGrade(66)
+Matthew.addGrade(60)
+Matthew.addGrade(79)
+
+console.log("La media di Matteo è: " + Matthew.average() + ".")
--- array/words.js
+++ array/words.js
@@ -1,1 +1,9 @@
+var words = ["Ciao", "a", "tutti","come", "va", "?"]
+var copiedWords = JSON.parse(JSON.stringify(words));
 
+var wordsNoCommas = words.join()
+var reversedWords = copiedWords.reverse();
+var reversedWordsNoCommas = reversedWords.join();
+
+
+console.log(wordsNoCommas, reversedWordsNoCommas)
