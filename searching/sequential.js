034e79dc546a938e339ee944a9a60f805f132c2f
matthingly-git <matthew.cesar96@gmail.com>
2020-03-16 00:10:53 +0100

    Ok

--- searching/sequential.js
+++ searching/sequential.js
@@ -1,1 +1,11 @@
+function seqSearch(arr, data) {
+  for (var i = arr.length; i >= 0; --i) {
+    console.log(i)
+    if (arr[i] == data) {
+      return i;
+    }
+  }
+  return false;
+}
 
+console.log(seqSearch([1, 2, 1, 4, 3, 4, 1], 1))
