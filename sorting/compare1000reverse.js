6ab1802cccb8f947e9769aebacaf97d8b856cb6c
matthingly-git <matthew.cesar96@gmail.com>
2020-03-18 18:38:16 +0100

    Ok

--- sorting/compare1000reverse.js
+++ sorting/compare1000reverse.js
@@ -1,1 +1,108 @@
+function CArray(numElements, dataStore) {
+  this.dataStore = dataStore;
+  this.pos = 0;
+  this.numElements = numElements;
+  this.insert = insert;
+  this.toString = toString;
+  this.clear = clear;
+  this.setData = setData;
+  this.swap = swap;
+  this.bubbleSort = bubbleSort
+  this.selectionSort = selectionSort
+  this.insertionSort = insertionSort
+}
+function setData() {
+  for (var i = 0; i < this.numElements; ++i) {
+    this.dataStore[i] = Math.floor(Math.random() *
+      (this.numElements + 1));
+  }
+}
+function clear() {
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    this.dataStore[i] = 0;
+  }
+}
+function insert(element) {
+  this.dataStore[this.pos++] = element;
+}
+function toString() {
+  var retstr = "";
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    retstr += this.dataStore[i] + " ";
+    if (i > 0 && i % 10 == 0) {
+      retstr += "\n";
+    }
+  }
+  return retstr;
+}
+function swap(arr, index1, index2) {
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
+function bubbleSort() {
+  var numElements = this.dataStore.length;
+  var temp;
+  for (var outer = numElements; outer >= 2; --outer) {
+    for (var inner = 0; inner <= outer - 1; ++inner) {
+      if (this.dataStore[inner] > this.dataStore[inner + 1]) {
+        swap(this.dataStore, inner, inner + 1);
+      }
+    }
+  }
+}
+function selectionSort() {
+  var min, temp;
+  for (var outer = 0; outer <= this.dataStore.length - 2; ++outer) {
+    min = outer;
+    for (var inner = outer + 1;
+      inner <= this.dataStore.length - 1; ++inner) {
+      if (this.dataStore[inner] < this.dataStore[min]) {
+        min = inner;
+      }
+    }
+    swap(this.dataStore, outer, min);
+  }
+}
+function insertionSort() {
+  var temp, inner;
+  for (var outer = 1; outer <= this.dataStore.length - 1; ++outer) {
+    temp = this.dataStore[outer];
+    inner = outer;
+    while (inner > 0 && (this.dataStore[inner - 1] >= temp)) {
+      this.dataStore[inner] = this.dataStore[inner - 1];
+      --inner;
+    }
+    this.dataStore[inner] = temp;
+  }
+}
 
+let oneHundNumsReverse = []
+for (let i = 1000; i > 0; i--) {
+  oneHundNumsReverse.push(i)
+}
+
+var numElements = oneHundNumsReverse.length;
+var nums = new CArray(numElements, oneHundNumsReverse);
+
+var start = new Date().getTime();
+nums.bubbleSort()
+var stop = new Date().getTime();
+var elapsed = stop - start;
+console.log("Elapsed time for the bubble sort on " + numElements + " elements is: " + elapsed + " milliseconds.");
+
+start = new Date().getTime();
+nums.selectionSort();
+stop = new Date().getTime();
+elapsed = stop - start;
+console.log("Elapsed time for the selection sort on " + numElements + " elements is: " + elapsed + " milliseconds.");
+
+start = new Date().getTime();
+nums.insertionSort();
+stop = new Date().getTime();
+elapsed = stop - start;
+console.log("Elapsed time for the insertion sort on " + numElements + " elements is: " + elapsed + " milliseconds.");
+
+//il bubble sort è sempre quello con tempo maggiore di tutti (tra gli 8 e i 9 ms);
+//il selection sort è al secondo posto (dai 2 ai 3 ms);
+//l'insertion sort è il più veloce (quasi sempre 0 ms, al massimo 1 ms).
