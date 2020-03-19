711af13bf779f85fa077e190740a06798f2d2f10
matthingly-git <matthew.cesar96@gmail.com>
2020-03-19 16:20:36 +0100

    Ok

--- objects/isEmpty.js
+++ objects/isEmpty.js
@@ -1,9 +1,18 @@
 function isEmpty(obj) {
+  let result = true
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
-
+      result = false
     }
   }
+  return result
 }
 
-//?????????
+
+let emptyObj = {}
+
+let notEMptyObj = {
+  name: "Matthew"
+}
+
+console.log(isEmpty(emptyObj), isEmpty(notEMptyObj))
